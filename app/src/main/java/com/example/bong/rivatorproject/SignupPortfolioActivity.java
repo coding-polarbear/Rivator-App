a9a988cc626bf80c7bec9623db56bcc332d2285e
baehyeonbin <baehynebin@hotmail.co.kr>
2017-05-27 15:06:13 +0900

    add all files in Rivator

---
+++ app/src/main/java/com/example/bong/rivatorproject/SignupPortfolioActivity.java
@@ -0,0 +1,47 @@
+package com.example.bong.rivatorproject;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by Bong on 2017-05-24.
+ */
+
+public class SignupPortfolioActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_portfolio);
+    }
+
+
+    /*
+    *커스텀 액션바
+    *setimageview, setbackground 등등 id를 받아온후 사용자 정의로 사용
+    */
+
+    private void setCustomActionbar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        getSupportActionBar().setElevation(0);
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_actionbar_type_cancle, null);
+        actionBar.setCustomView(mCustomView);
+
+        //레이어 색깔
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(mCustomView, params);
+
+    }
+}
+
