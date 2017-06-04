a8290eec614269f1fd5fc6f72501be6f375ac13d
baehyeonbin <baehynebin@hotmail.co.kr>
2017-06-04 23:58:26 +0900

    add main & select activity

--- app/src/main/java/com/example/bong/rivatorproject/MainActivity.java
+++ app/src/main/java/com/example/bong/rivatorproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.bong.rivatorproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSignIn(View view) {
+    }
+
+    public void goToSignUp(View view) {
+        Intent i = new Intent(getApplicationContext(),SelectActivity.class);
+        startActivity(i);
+
     }
 }
---
+++ app/src/main/java/com/example/bong/rivatorproject/SelectActivity.java
@@ -0,0 +1,13 @@
+package com.example.bong.rivatorproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+}
