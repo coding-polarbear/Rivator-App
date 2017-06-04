1436ede55d95cb19d3f599a75b60521b385a24c5
baehyeonbin <baehynebin@hotmail.co.kr>
2017-06-05 04:52:11 +0900

    add Teacher SignUp UI

--- app/src/main/java/com/example/bong/rivatorproject/SelectActivity.java
+++ app/src/main/java/com/example/bong/rivatorproject/SelectActivity.java
@@ -1,7 +1,9 @@
 package com.example.bong.rivatorproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+    }
+
+    public void goToTeacherSignup(View view) {
+        Intent i = new Intent(this, TeacherSignUpActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/example/bong/rivatorproject/TeacherSignUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.bong.rivatorproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_sign_up);
+    }
+}
