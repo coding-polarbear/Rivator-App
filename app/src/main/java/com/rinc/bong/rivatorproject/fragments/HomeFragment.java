3916fcf6b87d432256d6f1ac6196b61da0687085
baehyeonbin <baehynebin@hotmail.co.kr>
2017-07-16 04:35:16 +0900

    리스트뷰를 위한 사전 작업

--- app/src/main/java/com/rinc/bong/rivatorproject/fragments/HomeFragment.java
+++ app/src/main/java/com/rinc/bong/rivatorproject/fragments/HomeFragment.java
@@ -2,14 +2,17 @@ package com.rinc.bong.rivatorproject.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.rinc.bong.rivatorproject.R;
 
 
 public class HomeFragment extends Fragment {
+    private TextView moreText;
     public HomeFragment(){
 
     }
@@ -17,6 +20,9 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, null);
+        moreText = (TextView) view.findViewById(R.id.moreText);
+        moreText.setText(Html.fromHtml("<u>더보기<u>"));
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
