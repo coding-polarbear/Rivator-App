333e1fd9ddca058a140fc6b4b77b60013f2d9dad
baehynebin <baehynebin@hotmail.co.kr>
2017-09-12 23:17:28 +0900

    토큰 필요한 레트로핏 객체를 가져오기 위한 레트로핏 객체 추가

--- app/src/main/java/com/rinc/bong/rivatorproject/utils/RetrofitUtil.java
+++ app/src/main/java/com/rinc/bong/rivatorproject/utils/RetrofitUtil.java
@@ -2,11 +2,18 @@ package com.rinc.bong.rivatorproject.utils;
 
 import android.support.annotation.NonNull;
 
-import java.io.File;
+import com.rinc.bong.rivatorproject.beans.Token;
 
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -15,6 +22,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class RetrofitUtil {
+
     public static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://n0rr.iptime.org:7001")
             .addConverterFactory(GsonConverterFactory.create())
@@ -22,6 +30,26 @@ public class RetrofitUtil {
 
     public static final String MULTIPART_FORM_DATA = "multipart/form-data";
 
+    public static Retrofit getLoginRetrofit() {
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addInterceptor(chain -> {
+            Request original = chain.request();
+            Token token = Token.last(Token.class);
+            Request request = original.newBuilder()
+                    .header("authorization",token.getToken())
+                    .method(original.method(), original.body())
+                    .build();
+            return chain.proceed(request);
+        });
+
+        OkHttpClient client = httpClient.build();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://n0rr.iptime.org:7001")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+        return retrofit;
+    }
     public static MultipartBody.Part  createRequestBody(@NonNull File file) {
 
         RequestBody mFile = RequestBody.create(MediaType.parse("images/*"),file);
@@ -29,7 +57,6 @@ public class RetrofitUtil {
         return fileToUpload;
     }
     public static RequestBody createRequestBody(@NonNull String value) {
-
         return RequestBody.create(MediaType.parse(MULTIPART_FORM_DATA),value);
     }
 }
