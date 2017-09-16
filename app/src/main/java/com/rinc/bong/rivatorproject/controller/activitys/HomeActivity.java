8f506e7e40890861d79e403858cc298d3e1dca53
bhy98528 <bhy98528@gmail.com>
2017-09-17 04:46:21 +0900

    HomeActivity - SearchView Create, Style - NoActionBar Style Create, activity_home - Toolbar add

--- app/src/main/java/com/rinc/bong/rivatorproject/controller/activitys/HomeActivity.java
+++ app/src/main/java/com/rinc/bong/rivatorproject/controller/activitys/HomeActivity.java
@@ -2,13 +2,20 @@ package com.rinc.bong.rivatorproject.controller.activitys;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.speech.RecognizerIntent;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
@@ -17,35 +24,64 @@ import com.rinc.bong.rivatorproject.R;
 import com.rinc.bong.rivatorproject.controller.fragments.HomeFragment;
 import com.rinc.bong.rivatorproject.controller.fragments.ProfileFragment;
 
+import java.util.ArrayList;
+
+
 public class HomeActivity extends AppCompatActivity {
+
     private BottomNavigationViewEx bottomNavigationView;
     private Fragment fragment;
-    private ActionBar actionBar;
+    private Toolbar toolbar;
+    private SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        bottomNavigationView = (BottomNavigationViewEx) findViewById(R.id.bottomNavigation);
-        //BottomNavigationView 설정
+        init();
         setBottomNavigationView();
-        if(savedInstanceState == null) loadFragment(new HomeFragment(0));
+        if (savedInstanceState == null) loadFragment(new HomeFragment(0));
 
         //액션바 설정
-        setActionBar();
-
         //FCM에서 토픽 구독
+        setSearchView();
         FirebaseMessaging.getInstance().subscribeToTopic("news");
         FirebaseInstanceId.getInstance().getToken();
     }
 
-    private void setActionBar() {
-        actionBar = getSupportActionBar();
-        actionBar.setElevation(0);
-        actionBar.setTitle("홈");
+
+    private void setSearchView() {
+        setSupportActionBar(toolbar);
+        toolbar.inflateMenu(R.menu.main_menu_actionbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+
+
+        searchView = (SearchView) toolbar.getMenu().findItem(R.id.action_search).getActionView();
+        toolbar.getMenu().findItem(R.id.action_search).setIcon(R.drawable.search);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+
     }
+
+    private void init() {
+        bottomNavigationView = (BottomNavigationViewEx) findViewById(R.id.bottomNavigation);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
+
+
     private void loadFragment(Fragment fragment) {
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.main_container,fragment).commit();
+        transaction.replace(R.id.main_container, fragment).commit();
     }
 
     private void setBottomNavigationView() {
@@ -62,34 +98,34 @@ public class HomeActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
             int id = item.getItemId();
             int position = 0;
-            switch(id) {
+            switch (id) {
                 case R.id.btnHome:
                     position = 0;
                     fragment = new HomeFragment(position);
-                    actionBar.setTitle("홈");
+                    toolbar.setTitle("홈");
                     loadFragment(fragment);
                     break;
                 case R.id.btnTeacher:
                     position = 1;
                     fragment = new HomeFragment(position);
-                    actionBar.setTitle("강사");
+                    toolbar.setTitle("강사");
                     loadFragment(fragment);
                     break;
                 case R.id.btnCalendar:
                     position = 2;
-                    Intent i = new Intent(getApplicationContext(),CalendarActivity.class);
+                    Intent i = new Intent(getApplicationContext(), CalendarActivity.class);
                     startActivity(i);
                     break;
                 case R.id.btnCurrentLecture:
                     position = 3;
                     fragment = new HomeFragment(position);
-                    actionBar.setTitle("진행중 강좌");
+                    toolbar.setTitle("진행중 강좌");
                     loadFragment(fragment);
                     break;
                 case R.id.btnProfile:
                     position = 4;
                     fragment = new ProfileFragment();
-                    actionBar.setTitle("프로필");
+                    toolbar.setTitle("프로필");
                     loadFragment(fragment);
                     break;
             }
@@ -99,7 +135,16 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu_actionbar,menu);
+        getMenuInflater().inflate(R.menu.main_menu_actionbar, menu);
         return true;
+    }
+
+    public void onBackPressed() {
+        if (searchView.isFocused()) {
+            searchView.setIconified(false);
+            searchView.clearFocus();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
